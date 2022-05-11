d33b2e374c53d3949b59f4c67f4b81711f35b105
Erika Robles Araya <12820548+ekra26@users.noreply.github.com>
2022-05-10 20:26:54 -0400

    Delete RemoveStringSpaces.js

--- RemoveStringSpaces.js
+++
@@ -1,8 +0,0 @@
-//Simple, remove the spaces from the string, then return the resultant string.//
-
-
-function noSpace(x){
-  return x.replace(/\s/g, '');
-}
-
-//\s is the regex for "whitespace", and g is the "global" flag, meaning match ALL \s (whitespaces)//
